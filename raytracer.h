b70b985184f743c4316b56ebf1fe92419f06b3b9
albscui <albscui@gmail.com>
2018-03-15 20:22:42 -0400

    Autoformat with LLVM_4spaces, convert tabs to spaces

--- raytracer.h
+++ raytracer.h
@@ -1,38 +1,40 @@
 /***********************************************************
-	
-	Starter code for Assignment 3
-	
-	This file contains the interface and datastructures of the raytracer.  
-	Simple traversal and addition code to the datastructures are given to you.
+        
+
+        Starter code for Assignment 3
+        
+
+        This file contains the interface and datastructures of the raytracer.
+        Simple traversal and addition code to the datastructures are given to
+you.
 
 ***********************************************************/
 #pragma once
 
-#include "util.h"
-#include "scene_object.h"
 #include "light_source.h"
+#include "scene_object.h"
+#include "util.h"
 
 class Raytracer {
-public:
-	// Renders 3D scene to an image given camera and lights setup.
-	void render(Camera& camera, Scene& scene, LightList& light_list, Image& image);
-		
-private:
+  public:
+    // Renders 3D scene to an image given camera and lights setup.
+    void render(Camera &camera, Scene &scene, LightList &light_list,
+                Image &image);
 
-	// Return the color of the ray after intersection and shading, call 
-	// this function recursively for reflection and refraction.  
-	Color shadeRay(Ray3D& ray, Scene& scene, LightList& light_list);
+  private:
+    // Return the color of the ray after intersection and shading, call
+    // this function recursively for reflection and refraction.
+    Color shadeRay(Ray3D &ray, Scene &scene, LightList &light_list);
 
-	// Traversal code for the scene, the ray is transformed into 
-	// the object space of each node where intersection is performed.
-	void traverseScene(Scene& scene, Ray3D& ray);
+    // Traversal code for the scene, the ray is transformed into
+    // the object space of each node where intersection is performed.
+    void traverseScene(Scene &scene, Ray3D &ray);
 
-	// After intersection, calculate the color of the ray by shading it
-	// with all light sources in the scene.
-	void computeShading(Ray3D& ray, LightList& light_list);
+    // After intersection, calculate the color of the ray by shading it
+    // with all light sources in the scene.
+    void computeShading(Ray3D &ray, LightList &light_list);
 
-	// Precompute the modelToWorld and worldToModel transformations for each
+    // Precompute the modelToWorld and worldToModel transformations for each
     // object in the scene.
-	void computeTransforms(Scene& scene);
-
+    void computeTransforms(Scene &scene);
 };
--- scene_object.cpp
+++ scene_object.cpp
@@ -1,136 +1,135 @@
 /***********************************************************
-	
-	Starter code for Assignment 3
-	
-	Implements scene_object.h
+        
+
+        Starter code for Assignment 3
+        
+
+        Implements scene_object.h
 
 ***********************************************************/
 
-#include <cmath>
 #include "scene_object.h"
+#include <cmath>
 
-bool UnitSquare::intersect(Ray3D& ray, const Matrix4x4& worldToModel,
-		const Matrix4x4& modelToWorld) {
-	// TODO: implement intersection code for UnitSquare, which is
-	// defined on the xy-plane, with vertices (0.5, 0.5, 0), 
-	// (-0.5, 0.5, 0), (-0.5, -0.5, 0), (0.5, -0.5, 0), and normal
-	// (0, 0, 1).
-	//
-	// Your goal here is to fill ray.intersection with correct values
-	// should an intersection occur.  This includes intersection.point, 
-	// intersection.normal, intersection.none, intersection.t_value.   
-	//
-	// HINT: Remember to first transform the ray into object space  
-	// to simplify the intersection test.
+bool UnitSquare::intersect(Ray3D &ray, const Matrix4x4 &worldToModel,
+                           const Matrix4x4 &modelToWorld) {
+    // TODO: implement intersection code for UnitSquare, which is
+    // defined on the xy-plane, with vertices (0.5, 0.5, 0),
+    // (-0.5, 0.5, 0), (-0.5, -0.5, 0), (0.5, -0.5, 0), and normal
+    // (0, 0, 1).
+    //
+    // Your goal here is to fill ray.intersection with correct values
+    // should an intersection occur.  This includes intersection.point,
+    // intersection.normal, intersection.none, intersection.t_value.
+    //
+    // HINT: Remember to first transform the ray into object space
+    // to simplify the intersection test.
 
-    // Since the normal of the unit square is (0, 0, 1), 
+    // Since the normal of the unit square is (0, 0, 1),
     // we only need to consider the z coordinate.
-    double t =  -ray.origin[2] / ray.dir[2];
+    double t = -ray.origin[2] / ray.dir[2];
 
     // Calculate the point where the ray intersects the xy-plane.
-    Point3D point = ray.origin + t*ray.dir;
+    Point3D point = ray.origin + t * ray.dir;
     Intersection intersection;
 
     // Check if the point of intersection is within the unit square.
-    if(-0.5 <= point[0] <= 0.5 && -0.5 <= point[1] <= 0.5 && point[2] == 0){
+    if (-0.5 <= point[0] <= 0.5 && -0.5 <= point[1] <= 0.5 && point[2] == 0) {
         intersection.point = point;
-        intersection.normal = Vector3D(0,0,1);
+        intersection.normal = Vector3D(0, 0, 1);
         intersection.t_value = t;
         intersection.none = false;
     } else {
         intersection.none = true;
     }
 
-	return false;
+    return false;
 }
 
-bool UnitSphere::intersect(Ray3D& ray, const Matrix4x4& worldToModel,
-		const Matrix4x4& modelToWorld) {
-	// TODO: implement intersection code for UnitSphere, which is centred 
-	// on the origin.  
-	//
-	// Your goal here is to fill ray.intersection with correct values
-	// should an intersection occur.  This includes intersection.point, 
-	// intersection.normal, intersection.none, intersection.t_value.   
-	//
-	// HINT: Remember to first transform the ray into object space  
-	// to simplify the intersection test.
+bool UnitSphere::intersect(Ray3D &ray, const Matrix4x4 &worldToModel,
+                           const Matrix4x4 &modelToWorld) {
+    // TODO: implement intersection code for UnitSphere, which is centred
+    // on the origin.
+    //
+    // Your goal here is to fill ray.intersection with correct values
+    // should an intersection occur.  This includes intersection.point,
+    // intersection.normal, intersection.none, intersection.t_value.
+    //
+    // HINT: Remember to first transform the ray into object space
+    // to simplify the intersection test.
 
-	return false;
+    return false;
 }
 
 void SceneNode::rotate(char axis, double angle) {
-	Matrix4x4 rotation;
-	double toRadian = 2*M_PI/360.0;
-	int i;
-	
-	for (i = 0; i < 2; i++) {
-		switch(axis) {
-			case 'x':
-				rotation[0][0] = 1;
-				rotation[1][1] = cos(angle*toRadian);
-				rotation[1][2] = -sin(angle*toRadian);
-				rotation[2][1] = sin(angle*toRadian);
-				rotation[2][2] = cos(angle*toRadian);
-				rotation[3][3] = 1;
-			break;
-			case 'y':
-				rotation[0][0] = cos(angle*toRadian);
-				rotation[0][2] = sin(angle*toRadian);
-				rotation[1][1] = 1;
-				rotation[2][0] = -sin(angle*toRadian);
-				rotation[2][2] = cos(angle*toRadian);
-				rotation[3][3] = 1;
-			break;
-			case 'z':
-				rotation[0][0] = cos(angle*toRadian);
-				rotation[0][1] = -sin(angle*toRadian);
-				rotation[1][0] = sin(angle*toRadian);
-				rotation[1][1] = cos(angle*toRadian);
-				rotation[2][2] = 1;
-				rotation[3][3] = 1;
-			break;
-		}
-		if (i == 0) {
-			this->trans = this->trans*rotation; 	
-			angle = -angle;
-		} 
-		else {
-			this->invtrans = rotation*this->invtrans; 
-		}	
-	}
+    Matrix4x4 rotation;
+    double toRadian = 2 * M_PI / 360.0;
+    int i;
+
+    for (i = 0; i < 2; i++) {
+        switch (axis) {
+        case 'x':
+            rotation[0][0] = 1;
+            rotation[1][1] = cos(angle * toRadian);
+            rotation[1][2] = -sin(angle * toRadian);
+            rotation[2][1] = sin(angle * toRadian);
+            rotation[2][2] = cos(angle * toRadian);
+            rotation[3][3] = 1;
+            break;
+        case 'y':
+            rotation[0][0] = cos(angle * toRadian);
+            rotation[0][2] = sin(angle * toRadian);
+            rotation[1][1] = 1;
+            rotation[2][0] = -sin(angle * toRadian);
+            rotation[2][2] = cos(angle * toRadian);
+            rotation[3][3] = 1;
+            break;
+        case 'z':
+            rotation[0][0] = cos(angle * toRadian);
+            rotation[0][1] = -sin(angle * toRadian);
+            rotation[1][0] = sin(angle * toRadian);
+            rotation[1][1] = cos(angle * toRadian);
+            rotation[2][2] = 1;
+            rotation[3][3] = 1;
+            break;
+        }
+        if (i == 0) {
+            this->trans = this->trans * rotation;
+            angle = -angle;
+        } else {
+            this->invtrans = rotation * this->invtrans;
+        }
+    }
 }
 
 void SceneNode::translate(Vector3D trans) {
-	Matrix4x4 translation;
-	
-	translation[0][3] = trans[0];
-	translation[1][3] = trans[1];
-	translation[2][3] = trans[2];
-	this->trans = this->trans*translation; 	
-	translation[0][3] = -trans[0];
-	translation[1][3] = -trans[1];
-	translation[2][3] = -trans[2];
-	this->invtrans = translation*this->invtrans; 
+    Matrix4x4 translation;
+
+    translation[0][3] = trans[0];
+    translation[1][3] = trans[1];
+    translation[2][3] = trans[2];
+    this->trans = this->trans * translation;
+    translation[0][3] = -trans[0];
+    translation[1][3] = -trans[1];
+    translation[2][3] = -trans[2];
+    this->invtrans = translation * this->invtrans;
 }
 
-void SceneNode::scale(Point3D origin, double factor[3] ) {
-	Matrix4x4 scale;
-	
-	scale[0][0] = factor[0];
-	scale[0][3] = origin[0] - factor[0] * origin[0];
-	scale[1][1] = factor[1];
-	scale[1][3] = origin[1] - factor[1] * origin[1];
-	scale[2][2] = factor[2];
-	scale[2][3] = origin[2] - factor[2] * origin[2];
-	this->trans = this->trans*scale; 	
-	scale[0][0] = 1/factor[0];
-	scale[0][3] = origin[0] - 1/factor[0] * origin[0];
-	scale[1][1] = 1/factor[1];
-	scale[1][3] = origin[1] - 1/factor[1] * origin[1];
-	scale[2][2] = 1/factor[2];
-	scale[2][3] = origin[2] - 1/factor[2] * origin[2];
-	this->invtrans = scale*this->invtrans; 
+void SceneNode::scale(Point3D origin, double factor[3]) {
+    Matrix4x4 scale;
+
+    scale[0][0] = factor[0];
+    scale[0][3] = origin[0] - factor[0] * origin[0];
+    scale[1][1] = factor[1];
+    scale[1][3] = origin[1] - factor[1] * origin[1];
+    scale[2][2] = factor[2];
+    scale[2][3] = origin[2] - factor[2] * origin[2];
+    this->trans = this->trans * scale;
+    scale[0][0] = 1 / factor[0];
+    scale[0][3] = origin[0] - 1 / factor[0] * origin[0];
+    scale[1][1] = 1 / factor[1];
+    scale[1][3] = origin[1] - 1 / factor[1] * origin[1];
+    scale[2][2] = 1 / factor[2];
+    scale[2][3] = origin[2] - 1 / factor[2] * origin[2];
+    this->invtrans = scale * this->invtrans;
 }
-
-
